3c79a52d83d690e0e360301b9798e3fe9c885784
j2itu <jai22034@gmail.com>
2019-07-15 13:14:46 +0530

    In this the offer and looking for screens are created by me 15/06/2019

---
+++ Nasscom/src/components/LookingFor/LookingStyle.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+
+    SectionHeaderStyle: {
+        backgroundColor: '#f8f8f8',
+        borderBottomWidth: 1.2,
+        borderBottomColor: "#01ade6",
+        height: 40,
+        flexDirection: 'row',
+       
+    },
+
+    SectionListItemStyle: {
+        fontSize: 15,
+        padding: 5,
+        color: '#000',
+        backgroundColor: '#ffffff'
+
+    }
+});
+
+export default styles;
